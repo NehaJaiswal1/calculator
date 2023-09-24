211c4684f31de9a3a433bb10eca1bed586f495b2
Neha Jaiswal <nehajaiswal694@gmail.com>
2023-09-24 10:06:10 +0530

    css update

--- calculator/src/components/Calcu.jsx
+++ calculator/src/components/Calcu.jsx
@@ -36,40 +36,40 @@ function Calcu() {
         </div>
         <div className='mt-5 '>
           <div className='flex space-x-4'>
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950' onClick={handleClear}>C</button>
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950 ' onClick={() => { handleClick('%') }}>%</button>
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950' onClick={() => { handleClick('(') }}>(</button>
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950 '
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold  shadow-lg shadow-yellow-950' onClick={handleClear}>C</button>
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950 ' onClick={() => { handleClick('%') }}>%</button>
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950' onClick={() => { handleClick('(') }}>(</button>
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950 '
             onClick={() => { handleClick(')') }}>)</button>
           </div>
           <div className='flex space-x-4 mt-2'>
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950' onClick={() => { handleClick('7') }}>7</button>
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950'onClick={() => { handleClick('8') }}>8</button>
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950' onClick={() => { handleClick('7') }}>7</button>
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950'onClick={() => { handleClick('8') }}>8</button>
 
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950'onClick={() => { handleClick('9') }}>9</button>
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950'onClick={() => { handleClick('*') }}>*</button>
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950'onClick={() => { handleClick('9') }}>9</button>
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950'onClick={() => { handleClick('*') }}>*</button>
           </div>
           <div className='flex space-x-4 mt-2'>
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950'onClick={() => { handleClick('4') }}>4</button>
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950' onClick={() => { handleClick('5') }}>5</button>
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950 'onClick={() => { handleClick('6') }}>6</button>
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950 ' onClick={() => { handleClick('-') }}>-</button>
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950'onClick={() => { handleClick('4') }}>4</button>
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950' onClick={() => { handleClick('5') }}>5</button>
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950 'onClick={() => { handleClick('6') }}>6</button>
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950 ' onClick={() => { handleClick('-') }}>-</button>
           </div>
           <div className='flex space-x-4 mt-2'>
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950'onClick={() => { handleClick('1') }}>1</button>
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950'onClick={() => { handleClick('2') }}>2</button>
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950 ' onClick={() => { handleClick('3') }}>3</button>
-            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950'onClick={() => { handleClick('+') }}>+</button>
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950'onClick={() => { handleClick('1') }}>1</button>
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950'onClick={() => { handleClick('2') }}>2</button>
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950 ' onClick={() => { handleClick('3') }}>3</button>
+            <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950'onClick={() => { handleClick('+') }}>+</button>
           </div>
           <div className='flex space-x-4 mt-2'>
-          <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950 ' onClick={() => { handleClick('0') }}>0</button>
-          <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950 'onClick={() => { handleClick('.') }}>.</button>
-          <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950 ' onClick={() => { handleClick(',') }}>,</button>
-          <button className='bg-yellow-600 rounded-full w-20 h-8 p-2 shadow-lg shadow-yellow-950 ' onClick={() => { handlecalculate('=') }}>=</button>
+          <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950 ' onClick={() => { handleClick('0') }}>0</button>
+          <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950 'onClick={() => { handleClick('.') }}>.</button>
+          <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950 ' onClick={() => { handleClick(',') }}>,</button>
+          <button className='bg-yellow-600 rounded-full w-20 h-8 p-2  flex items-center justify-center font-bold shadow-lg shadow-yellow-950 ' onClick={() => { handlecalculate('=') }}>=</button>
           </div>
 
         </div>
-        <div className='flex space-x-4 mt-2 shadow-lg shadow-yellow-950 rounded-lg'>
+        <div className='flex space-x-4 mt-2 shadow-lg shadow-yellow-950 rounded-lg '>
           <p>Result: {result}</p>
         </div>
         </div>
